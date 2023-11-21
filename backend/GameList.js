dfaf0856abf7bac0f03ce8cd1ee95c1ab7613d96
BrittaneyRDouglas <127153896+BrittaneyRDouglas@users.noreply.github.com>
2023-11-20 21:17:17 -0500

    Update GameList.js

--- backend/GameList.js
+++ backend/GameList.js
@@ -1,1 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function GameList() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/games')
+      .then(response => setGames(response.data))
+      .catch(error => console.error('Error fetching games:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Game Reviews</h1>
+      <ul>
+        {games.map(game => (
+          <li key={game._id}>
+            <Link to={`/game/${game._id}`}>{game.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default GameList;
 
