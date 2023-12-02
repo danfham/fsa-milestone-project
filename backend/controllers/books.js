c1ef94885ece458ac6730449e77f78c81d0c7259
danfham <dan.cunningham91@icloud.com>
2023-12-02 10:07:35 -0500

    organizing views within the frontend folder

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const User = require('../models/User')
 const Books = require('../models/Book')
 
-// GET page of all itineraries
+// GET page of all 
 router.get('/', async (req,res) =>{
     const itineraries = await Books.find()
     res.render('index', {Book})
