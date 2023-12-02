6973a3db388e67ea768f49150b051b771cff51ec
BrittaneyRDouglas <127153896+BrittaneyRDouglas@users.noreply.github.com>
2023-12-02 12:08:04 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,14 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
