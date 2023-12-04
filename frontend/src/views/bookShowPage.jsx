ffcf6f933e1073f54794c094557d3d58faa1a33c
danfham <dan.cunningham91@icloud.com>
2023-12-04 12:52:52 -0500

    committing changes to frontend

--- frontend/src/views/bookShowPage.jsx
+++ frontend/src/views/bookShowPage.jsx
@@ -1,21 +1,45 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+
 function BookShowPage({book}) {
-    return (
-      <main>
-        <div>
-          <img src={book.cover} alt="Book Cover" />
-          <div>
-            <h1>{book.title}</h1>
-            <div>
-                <h2>Rating: {book.rating}/5.00</h2>
-                <button className="btn-primary">Submit Rating</button>
-            </div>
-          </div>
-        </div>
-        <div>
-            <h1>Reviews</h1>
-        </div>
-      </main>
-    );
+
+	const history = useNavigate()
+	
+	const [books, setBooks] = useState([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(`http://localhost:5000/book`)
+			const resData = await response.json()
+			setBooks(resData)
+		}
+		fetchData()
+	}, [])
+
+  
+	let booksFormatted = books.map((book) => {
+		return (
+			<div className="col-sm-6" key={book.bookId}>
+				<h2>
+					<a href="#" onClick={() => history.push(`/view/${book.bookId}`)} >
+						{book.title}
+					</a>
+				</h2>
+				<p className="text-center">
+					Written in {book.author}, {place.releaseDate}
+				</p>
+			</div>
+		)
+	})
+	return (
+		<main>
+			<h1>Library</h1>
+			<div className="row">
+				{booksFormatted}
+			</div>
+		</main>
+	)
 }
   
 export default BookShowPage;
