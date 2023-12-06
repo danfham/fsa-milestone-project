5474d8f092a5f447f4af7fe0c27a69d6271632ce
danfham <dan.cunningham91@icloud.com>
2023-12-06 12:27:42 -0500

    rough signin and login page done

--- views/users/LoginForm.jsx
+++ views/users/LoginForm.jsx
@@ -1,83 +1,24 @@
-import { useContext, useState } from "react"
-import { useHistory } from "react-router"
-import { CurrentUser } from "../contexts/CurrentUser"
+const React = require('react')
+const Default = require("../layouts/default")
 
 function LoginForm() {
 
-    const history = useHistory()
-
-    const { setCurrentUser } = useContext(CurrentUser)
-
-    const [credentials, setCredentials] = useState({
-        username: '',
-        password: ''
-    })
-
-    const [errorMessage, setErrorMessage] = useState(null)
-
-    async function handleSubmit(e) {
-        e.preventDefault()
-        const response = await fetch(``, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-
-    const data = await response.json()
-
-    if (response.status === 200) {
-        setCurrentUser(data.user)
-        localStorage.setItem('token', data.token)
-        history.push(`/`)
-    } else {
-        setErrorMessage(data.message)
-    }
-
-    }
-
     return (
-        <main>
-            <h1>Login</h1>
-            {errorMessage !== null
-                ? (
-                    <div className="alert alert-danger" role="alert">
-                        {errorMessage}
-                    </div>
-                )
-                : null
-            }
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            type="username"
-                            required
-                            value={credentials.username}
-                            onChange={e => setCredentials({ ...credentials, username: e.target.value })}
-                            className="form-control"
-                            id="username"
-                            name="username"
-                        />
-                    </div>
-                    <div className="">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            required
-                            value={credentials.password}
-                            onChange={e => setCredentials({ ...credentials, password: e.target.value })}
-                            className="form-control"
-                            id="password"
-                            name="password"
-                        />
-                    </div>
-                </div>
-                <input className="" type="submit" value="Login" />
-            </form>
-        </main>
+        <Default>
+            <h1>Login to your account</h1>
+            <main className='main-container'>
+                <form>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" />
+
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" />
+
+                    <button type="button" onclick="login()">Login</button>
+                </form>
+            </main>
+        </Default>
+
     )
 }
 
--- views/users/SignUpForm.jsx
+++ views/users/SignUpForm.jsx
@@ -1,72 +1,25 @@
-import { useState, useEffect } from "react"
-import { useHistory, useParams } from "react-router"
+const React = require('react')
+const Default = require("../layouts/default")
 
 function SignUpForm() {
 
-	const history = useHistory()
+    return (
+        <Default>
+            <h1>Login to your account</h1>
+            <main className='main-container'>
+                <form>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" />
 
-	const [user, setUser] = useState({
-		username: '',
-		password: ''
-	})
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" />
 
-	async function handleSubmit(e) {
-		e.preventDefault()
+                    <button type="button" onclick="login()">Login</button>
+                </form>
+            </main>
+        </Default>
 
-		await fetch(``, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(user)
-		})
-
-		history.push(`/`)
-	}
-
-	return (
-		<main>
-			<h1>Sign Up</h1>
-            {errorMessage !== null
-                ? (
-                    <div className="alert alert-danger" role="alert">
-                        {errorMessage}
-                    </div>
-                )
-                : null
-            }
-			<form onSubmit={handleSubmit}>
-				<div className="row">
-					<div className="">
-						<label htmlFor="username">Choose a Username</label>
-						<input
-							required
-							value={user.username}
-							onChange={e => setUser({ ...user, username: e.target.value })}
-							className="form-control"
-							id="username"
-							name="username"
-						/>
-                    </div>
-				</div>
-				<div className="row">
-					<div className="">
-						<label htmlFor="password">Password</label>
-						<input
-							type="password"
-							required
-							value={user.password}
-							onChange={e => setUser({ ...user, password: e.target.value })}
-							className="form-control"
-							id="password"
-							name="password"
-						/>
-					</div>
-				</div>
-				<input className="" type="submit" value="Sign Up" />
-			</form>
-		</main>
-	)
+    )
 }
 
-export default SignUpForm
+export default SignUpForm;
